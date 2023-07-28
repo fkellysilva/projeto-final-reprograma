ec01e9f44ef38a8ca733ff79a7b194b2e7cef979
Francielly Kelly <fransilva.fks@gmail.com>
2023-07-27 21:14:54 -0300

    make wallet unique

--- src/models/wallet.js
+++ src/models/wallet.js
@@ -17,7 +17,9 @@ const walletSchema = mongoose.Schema(
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+      ref: "User",
+      unique: true,
+      required: true,
     },
   },
   { timestamp: true }
